40acf2f31db3017207032f35e64358041f4fa11d
juan roberto rosales vargas <juanros13@gmail.com>
2021-04-09 18:48:58 -0500

    actualizacion tablas ultima version

--- src/main/java/gob/nucleo/plantaservice/Controller/ViveroRestController.java
+++ src/main/java/gob/nucleo/plantaservice/Controller/ViveroRestController.java
@@ -10,10 +10,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
+
+
+@RestController
 public class ViveroRestController {
 
     @Autowired
@@ -29,7 +33,7 @@ public class ViveroRestController {
     ){
         Map<String, Object> response = new HashMap<>();
         try{
-            response.put("beneficiario", viveroService.findByUsuarioCreo(pageNo, pageSize, sortBy, orderBy,idTecnico));
+            response.put("viveros", viveroService.findByUsuarioCreo(pageNo, pageSize, sortBy, orderBy,idTecnico));
             response.put("success", "true" );
             return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK) ;
         }catch (DataAccessException e){
